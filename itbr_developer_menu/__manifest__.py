219c919d83be2ace01f5b012a0b6b986bc99ac8e
Renan Teixeira <git@renanteixeira.com.br>
2026-08-21 23:13:02 -0300

    bkpln26: snapshot do trabalho nao commitado em 21/08/2026 23:13

---
+++ itbr_developer_menu/__manifest__.py
@@ -0,0 +1,30 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Developer Menu",
+    "version": "1.0.0",
+    "category": "Hidden/Tools",
+    "license": "AGPL-3",
+    "summary": "Menu Shortcut for developer usage",
+    "description": """
+Developer menu
+==============
+
+Add a menu which gather main technical used menus
+
+How to use it
+-------------
+
+Ensure you're in ERP manager group and go to configuration page
+near `Technical` menu
+    """,
+    "author": "IT Brasil",
+    "website": "https://itbrasil.com.br",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "base_menus.xml",
+    ],
+    "installable": True,
+}
